f87474acc3b2fb2b5df892f5214bb821546462a0
Animesh Seemendra <animeshseemendra1997@gmail.com>
2017-02-20 16:24:00 +0530

    Update Homepage.cpp

--- Homepage.cpp
+++ Homepage.cpp
@@ -15,6 +15,7 @@ class user
 			int month;
 			int year;
 };
+user ob1;
 class Thirdlife
 {
 		public:
@@ -23,30 +24,60 @@ class Thirdlife
 			int login();
 			int signup();	
 };
+int homepage()
+{
+	int c;
+	cout<<endl<<"1. Login"<<endl<<"2. Signup"<<endl<<"choice :";
+	cin>>c;
+	Thirdlife u;
+	if(c==1)
+	{
+		u.login();
+	}
+	if(c==2)
+	{
+		u.signup();
+	}
+	return 0;
+}
 int Thirdlife :: login()
 {
 	Thirdlife ob;
-	for(int i=0;i<15;i++)
-	{
-		cout<<endl;
-	}
-	cout<<setw<<(45)<<"Login"<<endl<<endl;
+	user ob2;
+	cout<<setw(45)<<"Login"<<endl<<endl;
+	cin.ignore();
 	cout<<setw(35)<<"Username :";
 	gets(ob.username);
 	cout<<endl<<setw(35)<<"Password :";
 	gets(ob.password);
+	ifstream hp1;
+	hp1.open(ob.username,ios :: out|ios :: binary);
+	if(hp1==0)
+	{
+		cout<<"XX WRONG DATA ENTERED XX";
+		homepage();
+	}
+	else
+		{
+			hp1.read((char*)&ob2,sizeof(ob2));
+			if(strcmp(ob.password,ob2.password)!=0)
+				{
+					cout<<endl<<"XX WRONG DATA ENTERED XX";
+					homepage();
+				}
+		}
 	cin.ignore();
 	return 0;
 }
 int Thirdlife :: signup()
 {	
 	char password[16];
-	user ob1;
 	//*signup:
 	cout<<setw(45)<<"Signup"<<endl<<endl;
 	cin.ignore();
 	cout<<setw(35)<<"Username :";
 	gets(ob1.username);
+	ofstream hp(ob1.username,ios::out|ios::binary);
 	cout<<endl<<setw(35)<<"Password :";
 	gets(ob1.password);
 	cout<<endl<<setw(35)<<"re-enter your password :";
@@ -77,7 +108,7 @@ int Thirdlife :: signup()
 									goto date;
 								}
 						}		
-			else if(ob1.month%2==0)
+					else if(ob1.month%2==0)
 				{
 					if(ob1.date<0 && ob1.date>30)
 						{	
@@ -86,11 +117,13 @@ int Thirdlife :: signup()
 							cin.ignore();
 							goto date;
 						}
+				}
 					if(ob1.month==2)
 						{
-							if(ob1.year%4 != 0 || (ob1.year%100 == 0 && ob1.year%400 != 0))
+							if((ob1.year%4!=0) && (ob1.year%100==0) || (ob1.year%400!=0))
 								{
-									if(ob1.date<0 && ob1.date>28)
+									
+									if(ob1.date<0 && ob1.date>29)
 										{
 											cout<<endl<<setw(35)<<"XX Wrong data entered XX";
 											cout<<endl<<setw(35)<<"Re-enter your Date of Birth";
@@ -99,7 +132,7 @@ int Thirdlife :: signup()
 										}
 								}
 							else
-								if(ob1.date<0 && ob1.date>29)
+								if(ob1.date<0 && ob1.date>28)
 										{
 											cout<<endl<<setw(35)<<"XX Wrong data entered XX";
 											cout<<endl<<setw(35)<<"Re-enter your Date of Birth";
@@ -108,7 +141,6 @@ int Thirdlife :: signup()
 										}
 							
 						}
-					}
 				}
 			else if(ob1.month>7 && ob1.month<=12)
 				{
@@ -134,22 +166,15 @@ int Thirdlife :: signup()
 						}
 				}
 	}
-	//*user.write((char*)&ob1,sizeof(ob1));
-		return 0;
+	hp.write((char*)&ob1,sizeof(ob1));
+	hp.close();
+	cout<<endl<<"Welcome :)";
+	cin.ignore();
+	homepage();
+	return 0;
 }
 int main()
 {
-	int c;
-	cout<<"1. Login"<<endl<<"2. Signup"<<endl<<"choice :";
-	cin>>c;
-	Thirdlife u;
-	if(c==1)
-	{
-		u.login();
-	}
-	if(c==2)
-	{
-		u.signup();
-	}
+	homepage();
 	return 0;
 }
