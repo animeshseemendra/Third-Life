0a8d3101d40a94e77797861ed0e1e63247abf0a7
Animesh Seemendra <animeshseemendra1997@gmail.com>
2017-02-20 16:23:32 +0530

    Create User.cpp

---
+++ User.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+#include<iostream>
+#include<iomanip>
+#include<cstring>
+#include<ctime>
+#include<stdio.h>
+#include<fstream>
+#include"Homepage.cpp"
+
+class news_feed();
+{
+
+};
+class messages
+{
+	
+}
+int user()
+{	
+	profile ob;
+	int choice;
+	cout<<setw(45)<<"Welcome to Third life "<<ob.name<<endl<<endl;
+	cout<<setw(35)<<"1. News Feed "<<endl;
+	cout<<setw(35)<<"2. Messages "<<endl;
+	cout<<endl<<setw(35)<<"Choice :";
+	cout<<endl<<setw(35);
+	cin>>choice;
+	cin.ignore();
+	
+		
+} 
